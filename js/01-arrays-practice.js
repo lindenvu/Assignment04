1d6996576d258481c01abc051b4ec8c67162f16c
Linden Vu <lvu@dexcom.com>
2018-03-06 21:41:37 -0800

    Update 0.1

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -1,9 +1,18 @@
+/*eslint-env browser*/
 //STEP 1
-
+var favMovies = ["Rocky I", "Rocky II", "Rocky III", "Rocky IV", "Rocky V"];
+window.console.log(favMovies[1]);
 //STEP 2
-
+var movies = new Array(5);
+movies[0] = "Rocky I";
+movies[1] = "Rocky II";
+movies[2] = "Rocky III";
+movies[3] = "Rocky IV";
+movies[4] = "Rocky V";
+window.console.log(movies[0]);
 //STEP 3
-
+movies[5] = "Rocky Balboa";
+window.console.log(movies.length);
 //STEP 4
 
 //STEP 5
