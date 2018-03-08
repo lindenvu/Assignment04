05a264ef83d3eabf104e3034cdb89553413f59e5
Linden Vu <lvu@dexcom.com>
2018-03-07 18:48:50 -0800

    Update 0.2

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -1,7 +1,8 @@
 /*eslint-env browser*/
+/*
 //STEP 1
-var favMovies = ["Rocky I", "Rocky II", "Rocky III", "Rocky IV", "Rocky V"];
-window.console.log(favMovies[1]);
+var movies = ["Rocky I", "Rocky II", "Rocky III", "Rocky IV", "Rocky V"];
+window.console.log(movies[1]);
 //STEP 2
 var movies = new Array(5);
 movies[0] = "Rocky I";
@@ -11,24 +12,125 @@ movies[3] = "Rocky IV";
 movies[4] = "Rocky V";
 window.console.log(movies[0]);
 //STEP 3
+var movies = new Array(5);
+movies[0] = "Rocky I";
+movies[1] = "Rocky II";
+movies[2] = "Rocky III";
+movies[3] = "Rocky IV";
+movies[4] = "Rocky V";
 movies[5] = "Rocky Balboa";
 window.console.log(movies.length);
 //STEP 4
-
-//STEP 5
-
+var movies =  [];
+movies[0] = "Rocky I";
+movies[1] = "Rocky II";
+movies[2] = "Rocky III";
+movies[3] = "Rocky IV";
+movies[4] = "Rocky V";
+delete movies[0];
+window.console.log(movies);
+//STEP 5 
+var movies =  [];
+movies[0] = "Rocky I";
+movies[1] = "Rocky II";
+movies[2] = "Rocky III";
+movies[3] = "Rocky IV";
+movies[4] = "Rocky V";
+movies[5] = "Rocky Balboa";
+movies[6] = "Creed";
+var i;
+for (i = 0; i < movies.length; i += 1) {
+    window.console.log(movies[i]);
+}
 //STEP 6
-
+var movies =  [];
+movies[0] = "Rocky I";
+movies[1] = "Rocky II";
+movies[2] = "Rocky III";
+movies[3] = "Rocky IV";
+movies[4] = "Rocky V";
+movies[5] = "Rocky Balboa";
+movies[6] = "Creed";
+var i;
+for (i in movies) {
+        window.console.log(movies[i]);
+}
 //STEP 7
-
+var movies =  [];
+movies[0] = "Rocky I";
+movies[1] = "Rocky II";
+movies[2] = "Rocky III";
+movies[3] = "Rocky IV";
+movies[4] = "Rocky V";
+movies[5] = "Rocky Balboa";
+movies[6] = "Creed";
+movies = movies.sort();
+var i;
+for (i in movies) {
+        window.console.log(movies[i]);
+}
 //STEP 8
-
+var movies =  [];
+movies[0] = "Rocky I";
+movies[1] = "Rocky II";
+movies[2] = "Rocky III";
+movies[3] = "Rocky IV";
+movies[4] = "Rocky V";
+movies[5] = "Rocky Balboa";
+movies[6] = "Creed";
+var leastFavMovies = ["Beverly Hills Chihuahua", "Beverly Hills Chihuahua 2", "Beverly Hills Chihuahua 3"];
+window.console.log("Movies I like:\n\n");
+var i;
+for (i = 0; i < movies.length; i += 1) {
+    window.console.log(movies[i]);
+}
+window.console.log("\nMovies I regret watching:\n\n");
+var x;
+for (x = 0; x < leastFavMovies.length; x += 1) {
+    window.console.log(leastFavMovies[x]);
+}
 //STEP 9
-
+var movies =  [];
+movies[0] = "Rocky I";
+movies[1] = "Rocky II";
+movies[2] = "Rocky III";
+movies[3] = "Rocky IV";
+movies[4] = "Rocky V";
+movies[5] = "Rocky Balboa";
+movies[6] = "Creed";
+var leastFavMovies = ["Beverly Hills Chihuahua", "Beverly Hills Chihuahua 2", "Beverly Hills Chihuahua 3"];
+movies = movies.concat(leastFavMovies);
+movies = movies.sort();
+movies = movies.reverse();
+window.console.log(movies);
 //STEP 10
-
-//STEP 11
-
+var movies =  [];
+movies[0] = "Rocky I";
+movies[1] = "Rocky II";
+movies[2] = "Rocky III";
+movies[3] = "Rocky IV";
+movies[4] = "Rocky V";
+movies[5] = "Rocky Balboa";
+movies[6] = "Creed";
+var leastFavMovies = ["Beverly Hills Chihuahua", "Beverly Hills Chihuahua 2", "Beverly Hills Chihuahua 3"];
+movies = movies.concat(leastFavMovies);
+movies = movies.sort();
+movies = movies.reverse();
+window.console.log(movies.pop());
+//STEP 11 */
+var movies =  [];
+movies[0] = "Rocky I";
+movies[1] = "Rocky II";
+movies[2] = "Rocky III";
+movies[3] = "Rocky IV";
+movies[4] = "Rocky V";
+movies[5] = "Rocky Balboa";
+movies[6] = "Creed";
+var leastFavMovies = ["Beverly Hills Chihuahua", "Beverly Hills Chihuahua 2", "Beverly Hills Chihuahua 3"];
+movies = movies.concat(leastFavMovies);
+movies = movies.sort();
+movies = movies.reverse();
+window.console.log(movies.shift());
 //STEP 12
 
 //STEP 13
